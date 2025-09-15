505bf34fea70e834fadfe5cc78d31c5132161e4e
Kaley White <kwhite@radformation.com>
2025-09-15 08:45:51 -0500

    Debug StreamlitSetPageConfigMustBeFirstCommandError

--- utils/__init__.py
+++ utils/__init__.py
@@ -7,8 +7,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 import streamlit as st
-from streamlit.errors import StreamlitSetPageConfigMustBeFirstCommandError
-
 
 pd.set_option('future.no_silent_downcasting', True)
 
@@ -204,8 +202,8 @@ def init_page(pg_title):
         try:
             st.set_page_config(page_title=pg_title, layout='wide')
             st.session_state['page_configured'] = True
-        except StreamlitSetPageConfigMustBeFirstCommandError:
-            pass
+        except Exception as e:
+            st.write(str(e))
 
 
 def show_data_srcs(pg_title='RA/QA KPIs', error_msg=None):
